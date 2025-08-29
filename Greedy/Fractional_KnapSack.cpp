edf287942f16f8257a7f35c1a67c8a58dc77dc84
Mho <ommtripathi046@gmail.com>
2025-08-30 00:45:17 +0530

    fractionalKnapsack

---
+++ Greedy/Fractional_KnapSack.cpp
@@ -0,0 +1,40 @@
+class Solution {
+  public:
+    double fractionalKnapsack(vector<int>& val, vector<int>& wt, int capacity) {
+        // code here
+        int n= wt.size();
+        
+        vector<pair<double,pair<int,int>>>profit;
+        
+        for(int i=0;i<n;i++)
+        {
+            double ratio = (double)val[i] / wt[i];
+            profit.push_back({ratio, {val[i], wt[i]}});
+            
+        }
+        
+        sort(profit.begin(),profit.end(),[](auto &a,auto &b){
+            return a.first > b.first;
+        });
+        
+        double mxval = 0.0;
+        
+        for(int i = 0;i<n;i++)
+        {
+            int value  = profit[i].second.first; 
+            int weight = profit[i].second.second;
+            
+            if(weight <= capacity)
+            {
+                mxval += value;
+                capacity -= weight;
+            }else{
+                mxval +=profit[i].first * capacity;
+                capacity = 0;
+            
+            }
+        }
+        
+        return mxval;
+    }
+};
